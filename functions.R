ebe3dfa6b9f1e038c0b186c6e476e9ca5cf1f9c1
aimeeas <aimeeas@gmail.com>
2024-08-23 17:51:07 +0200

    Reorganization and words correction

--- functions.R
+++ functions.R
@@ -4,16 +4,19 @@ add2 <- function(x, y) {
   x + y
 }
 
-## Example
 
-add2 (3, 5)
+add2 (3, 5) ## Example
+
+
 
 ## Extract all the numbers above 10 from a vector
 
 above10 <- function(x) {
   use <- x > 10 ## It will return a logical vector
-  x[use] ## Subseting the vector 'x' with the vector 'use'
+  x[use] ## Subsetting the vector 'x' with the vector 'use'
 }
+
+
 
 ## Extract all the numbers above an arbitrary number
 
@@ -22,11 +25,14 @@ above <- function(x, n) { ## We can also establish a default number to 'n' by do
   x[use]
 }
 
+
 ## Example
 
 x <- 1:20 
 
 above(x, 12)
+
+
 
 ## Calcute the mean of each column from a dataframe
 
@@ -38,15 +44,16 @@ columnmean <- function(y) {
   }
   means
 }
-        ## In this example first it was fund the number of columns from a dataframe/matrix
-        ## by calling 'ncol()'. After, with that value stored as 'nc', it was created a numeric
-        ## vector ('means') contening the same number of entrances as 'nc', wich at the beginning will have all
-        ## the elements as 0. Then, by executing a 'for loop' the mean of wich column it will be calculated.
-        ## The number of that the 'for loop' will run is equal to the number of columns (nc).
+        ## In this example first it was found the number of columns from a dataframe/matrix
+        ## by calling 'ncol()'.
+        ## After, with that value stored as 'nc', it was created a numeric vector ('means') containing
+        ## the same number of entrances as 'nc'(which at the beginning will have all the elements set as 0).
+        ## Then, by executing a 'for loop', the mean of which column it will be calculated.
+        ## In this case, the number of times that the 'for loop' will run is equal to the number of columns (nc).
         ## The values of the the vector 'means' will be now established by calculating the mean of each column in the database.
-        ## The index 'i' in the 'for loop' established as '1:nc' needs to be part of the subsetting of the
+        ## OBS: The index 'i' in the 'for loop' established as '1:nc' needs to be part of the subsetting of the
         ## value 'means', so it saves separately the mean of each column, cause if not it will return only the last value established for 'i'.
-        ## E.g, if 'i' has 6 values and the 'means' is not subsetted with [i], when I print 'means', only one value will be showed, which will be the last value found in the 'for loop's calcule. 
+        ## E.g, if 'i' has 6 values and the 'means' is not subsetted with [i], when someone prints 'means', only one value will be showed, which will be the last founded in the 'for loop's calculation. 
 
 columnmean <- function(y, removeNA = TRUE) {
   nc <- ncol(y) 
